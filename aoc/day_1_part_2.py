eacd4400433dbbb169c9074a98ed6ee77392729a
Jake Cyr <cyrjake@gmail.com>
2023-12-02 10:18:46 -0500

    Attempt

--- aoc/day_1_part_2.py
+++ aoc/day_1_part_2.py
@@ -1,6 +1,32 @@
 from helpers.read_file import read_file
 import re
 
+possible_digits: dict[str, int] = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+def get_first_digit_in_line(line: str, reversed: bool = False) -> str | None:
+    found_digits = []
+    for name, number in possible_digits.items():
+        if reversed:
+            name = name[::-1]
+        if name in line:
+            found_digits.append((name, number, line.index(name)))
+
+    if found_digits:
+        found_digits.sort(key=lambda x: x[2])
+        return found_digits[0][0]
+    return None
+
 
 def get_calibration_sum(puzzle_input: str) -> int:
     lines: list[str] = puzzle_input.split("\n")
@@ -9,12 +35,37 @@ def get_calibration_sum(puzzle_input: str) -> int:
     for line in lines:
         if not line.strip():
             continue
+
+        print(line)
+
+        first_digit_name: str | None = get_first_digit_in_line(line)
+        print("First digit name:", first_digit_name)
+        if first_digit_name:
+            line = line.replace(
+                first_digit_name, str(possible_digits[first_digit_name]), 1
+            )
+
+        reversed_line: str = line[::-1]
+
+        print("Reversed line:", reversed_line)
+        last_digit_name: str | None = get_first_digit_in_line(reversed_line, True)
+        print("Last digit name:", last_digit_name)
+        if last_digit_name:
+            reversed_line = reversed_line.replace(
+                last_digit_name, str(possible_digits[last_digit_name[::-1]]), 1
+            )
+
+        line = reversed_line[::-1]
+
+        print("Un-reversed Line:", line)
+
         digits: str = re.sub(r"[^0-9]", "", line)
 
         if len(digits) < 1:
             continue
 
         num: str = digits[0] + digits[-1]
+        print("Num:", num)
         sum += int(num)
 
     return sum
---
+++ tests/test_day_1_part_2.py
@@ -0,0 +1,80 @@
+from aoc import day_1_part_2
+
+
+def test_get_calibration_sum_with_all_digit_lines() -> None:
+    puzzle_input = """
+    1122
+    1111
+    1234
+    91212129
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 136
+
+
+def test_get_calibration_sum_with_spelled_out_digits() -> None:
+    puzzle_input = """
+    oneljfef789two
+    1ddfhjsu3ahdfu23
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 25
+
+
+def test_get_calibration_sum_with_spelled_overlapping_digits() -> None:
+    puzzle_input = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 281
+
+
+def test_empty_lines() -> None:
+    puzzle_input = """
+    
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 0
+
+
+def test_lines_with_only_words() -> None:
+    puzzle_input = """
+    hello world
+    python coding
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 0
+
+
+def test_lines_with_single_digits() -> None:
+    puzzle_input = """
+    1abc
+    xyz2
+    3xyz4
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 67
+
+
+def test_non_overlapping_spelled_out_digits() -> None:
+    puzzle_input = """
+    onexyzthree
+    twoabcseven
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 40
+
+
+def test_lines_with_special_characters_and_digits() -> None:
+    puzzle_input = """
+    1!@#$%^&*()2
+    three#$$%four
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 46
+
+
+def test_lines_with_digits_in_the_middle() -> None:
+    puzzle_input = """
+    axb5c6dz
+    eyf7gh8i
+    """
+    assert day_1_part_2.get_calibration_sum(puzzle_input) == 134
